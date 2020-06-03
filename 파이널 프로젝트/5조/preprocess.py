c840f9c9dd8297eca888a0854fd1cd7fa5f6108e
subong0508 <subong0508@gmail.com>
2020-06-03 13:24:03 +0900

    fin

--- 파이널 프로젝트/5조/preprocess.py
+++ 파이널 프로젝트/5조/preprocess.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pickle
 import joblib
 import warnings
+from sklearn.impute import SimpleImputer
 
 warnings.filterwarnings('ignore')
 
@@ -17,7 +18,7 @@ file_name = args.file_name
 df = pd.read_csv(file_name)
 
 for col in df.columns:
-    df[col] = df[col].replace("?", np.nan)
+    df[col] = df[col].replace("?", np.nan).astype(np.float)
 
 def y_check(df):
     return "class" in df.columns
@@ -27,44 +28,69 @@ def na_check(df):
 
 if na_check(df):
 
+    pre_imputer = SimpleImputer(np.nan, "median")
+    df['Attr29'].replace(0, min(df['Attr29'][df['Attr29'] != 0]))
     total_assets = pd.Series(np.exp(df['Attr29']), index=df['Attr29'].index)
+    total_assets = pre_imputer.fit_transform(total_assets.values.reshape(-1, 1)).reshape(-1,)
+    total_assets = pd.Series(total_assets, index=df['Attr29'].index)
+
     assert not bool(total_assets.isnull().sum())
 
     total_equity = df['Attr10'] * total_assets
+    total_equity = pre_imputer.fit_transform(total_equity.values.reshape(-1, 1)).reshape(-1, )
+    total_equity = pd.Series(total_equity, index=df['Attr29'].index)
     assert not bool(total_equity.isnull().sum())
 
     total_liabilities = total_assets - total_equity
     assert not bool(total_liabilities.isnull().sum())
 
     short_term_liabilities = df['Attr51'] * total_assets
+    short_term_liabilities = pre_imputer.fit_transform(short_term_liabilities.values.reshape(-1, 1)).reshape(-1, )
+    short_term_liabilities = pd.Series(short_term_liabilities, index=df['Attr29'].index)
     assert not bool(short_term_liabilities.isnull().sum())
 
     long_term_liabilities = total_liabilities - short_term_liabilities
     assert not bool(long_term_liabilities.isnull().sum())
 
     total_sales = pd.Series(np.array(total_assets) * np.array(df['Attr36']))
+    total_sales = pre_imputer.fit_transform(total_sales.values.reshape(-1, 1)).reshape(-1, )
+    total_sales = pd.Series(total_sales, index=df['Attr29'].index)
     assert not bool(total_sales.isnull().sum())
 
     sales = total_assets * df['Attr9'].values
     assert not bool(sales.isnull().sum())
 
     gross_profit = total_assets * df['Attr18']
+    gross_profit = pre_imputer.fit_transform(gross_profit.values.reshape(-1, 1)).reshape(-1, )
+    gross_profit = pd.Series(gross_profit, index=df['Attr29'].index)
     assert not bool(gross_profit.isnull().sum())
 
     EBIT = total_assets * df['Attr7']
+    EBIT = pre_imputer.fit_transform(EBIT.values.reshape(-1, 1)).reshape(-1, )
+    EBIT = pd.Series(EBIT, index=df['Attr29'].index)
     assert not bool(EBIT.isnull().sum())
 
     EBITDA = total_assets * df['Attr48']
+    EBITDA = pre_imputer.fit_transform(EBITDA.values.reshape(-1, 1)).reshape(-1, )
+    EBITDA = pd.Series(EBITDA, index=df['Attr29'].index)
     assert not bool(EBITDA.isnull().sum())
 
     retained_earnings = total_assets * df['Attr6']
+    retained_earnings = pre_imputer.fit_transform(retained_earnings.values.reshape(-1, 1)).reshape(-1, )
+    retained_earnings = pd.Series(retained_earnings, index=df['Attr29'].index)
     assert not bool(retained_earnings.isnull().sum())
 
     net_profit = total_assets * df['Attr1']
+    net_profit = pre_imputer.fit_transform(net_profit.values.reshape(-1, 1)).reshape(-1, )
+    net_profit = pd.Series(net_profit, index=df['Attr29'].index)
     assert not bool(net_profit.isnull().sum())
 
     working_capital = total_assets * df['Attr3']
     profit_on_sales = total_assets * df['Attr35']
+    working_capital = pre_imputer.fit_transform(working_capital.values.reshape(-1, 1)).reshape(-1, )
+    working_capital = pd.Series(working_capital, index=df['Attr29'].index)
+    profit_on_sales = pre_imputer.fit_transform(profit_on_sales.values.reshape(-1, 1)).reshape(-1, )
+    profit_on_sales = pd.Series(profit_on_sales, index=df['Attr29'].index)
     assert not bool(working_capital.isnull().sum())
     assert not bool(profit_on_sales.isnull().sum())
 
@@ -107,7 +133,7 @@ if na_check(df):
         df_imputed.columns = df.columns
     else:
         X = df
-        df_imputed = imputer.transform(X)
+        df_imputed = pd.DataFrame(imputer.transform(X))
         df_imputed.columns = df.columns
 
 
